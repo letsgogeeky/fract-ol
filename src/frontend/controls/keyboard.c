3557dc6d8c3618e7be766ce8150aeb64065a3256
Ramy <ramymohamedmousa@gmail.com>
2023-07-25 11:56:06 +0200

    keyboard bindings added for color and detail control f#27

--- src/frontend/controls/keyboard.c
+++ src/frontend/controls/keyboard.c
@@ -1,5 +1,45 @@
 #include "frontend.h"
 
+void	control_red(mlx_key_data_t key, t_fractol *env)
+{
+	if (key.modifier != MLX_CONTROL && env->color_scale->red < 255)
+		env->color_scale->red += 10;
+	else if (env->color_scale->red > 0)
+			env->color_scale->red -= 10;
+}
+
+void	control_green(mlx_key_data_t key, t_fractol *env)
+{
+	if (key.modifier != MLX_CONTROL && env->color_scale->green < 255)
+			env->color_scale->green += 10;
+	else if (env->color_scale->green > 0)
+			env->color_scale->green -= 10;
+}
+
+void	control_blue(mlx_key_data_t key, t_fractol *env)
+{
+	if (key.modifier != MLX_CONTROL && env->color_scale->blue < 255)
+			env->color_scale->blue += 10;
+	else if (env->color_scale->blue > 0)
+			env->color_scale->blue -= 10;
+}
+
+void	control_transparency(mlx_key_data_t key, t_fractol *env)
+{
+	if (key.modifier != MLX_CONTROL && env->color_scale->transparency < 255)
+			env->color_scale->transparency += 10;
+	else if (env->color_scale->transparency > 0)
+			env->color_scale->transparency -= 10;
+}
+
+void	control_details(mlx_key_data_t key, t_fractol *env)
+{
+	if (key.modifier != MLX_CONTROL)
+			env->estimator_max += 20;
+	else if (env->estimator_max > 30)
+			env->estimator_max -= 20;
+}
+
 void ft_key_hook(mlx_key_data_t key, void * param)
 {
 	t_fractol *env;
@@ -10,40 +50,14 @@ void ft_key_hook(mlx_key_data_t key, void * param)
 		puts("The SPACE key was pressed!");
 	}
 	else if (key.key == MLX_KEY_R)
-	{
-		if (env->color_scale->red < 255)
-		{
-			env->color_scale->red += 10;
-			compute_frame(env);
-			puts("The R key was pressed!");
-		}
-	}
+		control_red(key, env);
 	else if (key.key == MLX_KEY_G)
-	{
-		if (env->color_scale->green < 255)
-		{
-			env->color_scale->green += 10;
-			compute_frame(env);
-			puts("The G key was pressed!");
-			printf("new green: %d\n",env->color_scale->green);
-		}
-	}
+		control_green(key, env);
 	else if (key.key == MLX_KEY_B)
-	{
-		if (env->color_scale->blue < 255)
-		{
-			env->color_scale->blue += 10;
-			compute_frame(env);
-			puts("The B key was pressed!");
-		}
-	}
+		control_blue(key, env);
 	else if (key.key == MLX_KEY_A)
-	{
-		if (env->color_scale->transparency < 255)
-		{
-			env->color_scale->transparency += 10;
-			compute_frame(env);
-			puts("The A key was pressed!");
-		}
-	}
+		control_transparency(key, env);
+	else if (key.key == MLX_KEY_D)
+		control_details(key, env);
+	compute_frame(env);
 }
