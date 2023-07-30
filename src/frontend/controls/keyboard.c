4b7b49051112a33a2bd6c7f376123fd6e4f9de6e
Ramy <ramymohamedmousa@gmail.com>
2023-07-30 20:48:49 +0200

    keyboard arrows binding for f#35

--- src/frontend/controls/keyboard.c
+++ src/frontend/controls/keyboard.c
@@ -39,6 +39,55 @@ void	control_details(mlx_key_data_t key, t_fractol *env)
 	else if (env->estimator_max > 5)
 			env->estimator_max -= 5;
 }
+void	go_left(mlx_key_data_t key, t_fractol *env)
+{
+	double range;
+
+	if (key.action == MLX_RELEASE)
+	{
+		range = env->real_max - env->real_min;
+		env->real_min -= (range * 0.1);
+		env->real_max -= (range * 0.1);
+		compute_frame(env);
+	}
+		
+}
+void	go_right(mlx_key_data_t key, t_fractol *env)
+{
+	double range;
+	if (key.action != MLX_RELEASE)
+	{
+		range = env->real_max - env->real_min;
+		env->real_min += (range * 0.1);
+		env->real_max += (range * 0.1);
+		compute_frame(env);
+	}
+		
+}
+void	go_up(mlx_key_data_t key, t_fractol *env)
+{
+	double range;
+	if (key.action != MLX_RELEASE)
+	{
+		range = env->imaginary_max - env->imaginary_min;
+		env->imaginary_max += (range * 0.1);
+		env->imaginary_min += (range * 0.1);
+		compute_frame(env);
+	}
+		
+}
+void	go_down(mlx_key_data_t key, t_fractol *env)
+{
+	double range;
+	if (key.action != MLX_RELEASE)
+	{
+		range = env->imaginary_max - env->imaginary_min;
+		env->imaginary_max -= (range * 0.1);
+		env->imaginary_min -= (range * 0.1);
+		compute_frame(env);
+	}
+		
+}
 
 void ft_key_hook(mlx_key_data_t key, void * param)
 {
@@ -59,5 +108,13 @@ void ft_key_hook(mlx_key_data_t key, void * param)
 		control_transparency(key, env);
 	else if (key.key == MLX_KEY_D)
 		control_details(key, env);
+	else if (key.key == MLX_KEY_LEFT)
+		go_left(key, env);
+	else if (key.key == MLX_KEY_RIGHT)
+		go_right(key, env);
+	else if (key.key == MLX_KEY_UP)
+		go_up(key, env);
+	else if (key.key == MLX_KEY_DOWN)
+		go_down(key, env);
 	compute_frame(env);
 }
