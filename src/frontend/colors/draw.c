c3f07caceead4607f3d0100fe6ec57c45bebeb40
Ramy <ramymohamedmousa@gmail.com>
2023-08-01 03:12:29 +0200

    code formatting enhances

--- src/frontend/colors/draw.c
+++ src/frontend/colors/draw.c
@@ -4,24 +4,17 @@
 void ft_pixel(void *img, int x, int y, int32_t estimator, t_fractol *env)
 {
 	int color;
-	if (estimator == env->estimator_max)
-	{
-		mlx_put_pixel(img, x, y, 0x000000FF);
-	}
-	else
+
+	color = 0x000000FF;
+	if (estimator != env->estimator_max)
 	{
 		color = get_rgba((estimator * env->color_scale->red / env->estimator_max), \
-			(estimator * env->color_scale->green / env->estimator_max), (estimator * env->color_scale->blue / env->estimator_max),\
+			(estimator * env->color_scale->green / env->estimator_max), \
+			(estimator * env->color_scale->blue / env->estimator_max),\
 			(estimator * env->color_scale->transparency / env->estimator_max));
-		mlx_put_pixel(img, x, y, color * estimator);
+		color *= estimator;
 	}
-	// if (estimator != env->estimator_max)
-	// {
-	// 	color = get_rgba((estimator * env->color_scale->red / env->estimator_max), \
-	// 		(estimator * env->color_scale->green / env->estimator_max), (estimator * env->color_scale->blue / env->estimator_max),\
-	// 		(estimator * env->color_scale->transparency / env->estimator_max));
-	// 	mlx_put_pixel(img, x, y, color * estimator);
-	// }
+	mlx_put_pixel(img, x, y, color);
 }
 
 void compute_frame(t_fractol *env)
@@ -35,8 +28,6 @@ void compute_frame(t_fractol *env)
 
 	if (env->should_draw == false)
 		return ;
-	printf("compute frame\n");
-	fflush(stdout);
 	img = (mlx_image_t *)env->current_frame;
 	z = (t_complex *)malloc(sizeof(t_complex*));
 	c = (t_complex *)malloc(sizeof(t_complex*));
@@ -48,13 +39,9 @@ void compute_frame(t_fractol *env)
 		while (x < env->width)
 		{
 			if (env->f_type == JULIA)
-			{
 				n = compute_julia_pixel(env, z, c, x, y);
-			}
 			else if (env->f_type == MANDELBROT)
-			{
 				n = compute_mandelbrot_pixel(env, z, c, x, y);
-			}
 			ft_pixel(img, x, y, n, env);
 			x++;
 		}
--- src/frontend/controls/mouse.c
+++ src/frontend/controls/mouse.c
@@ -16,8 +16,6 @@ void mouse_scroll_hook(double delta_x, double delta_y, void * param)
     // env->zoom->factor = fabs(delta_y) * 50;
     // env->zoom->factor = 0.05;
     // printf("%f\n", delta_x);
-    printf("#");
-	fflush(stdout);
 	(void) delta_x;
     if (delta_y > 0)
         env->zoom->factor = 0.97;
@@ -31,43 +29,8 @@ void mouse_scroll_hook(double delta_x, double delta_y, void * param)
     env->real_max = interpolate(mouse_real, env->real_max, interpolation);
     env->imaginary_min = interpolate(mouse_imaginary, env->imaginary_min, interpolation);
     env->imaginary_max =  interpolate(mouse_imaginary, env->imaginary_max, interpolation);
-    // if(delta_y > 0)
-    // {
-    //     env->real_min -= ((env->real_max - env->real_min) * env->zoom->factor);
-    //     env->real_max += ((env->real_max - env->real_min) * env->zoom->factor);
-    //     env->imaginary_min -= ((env->imaginary_max- env->imaginary_min) * env->zoom->factor);
-    //     env->imaginary_max += ((env->imaginary_max- env->imaginary_min) * env->zoom->factor);
-    // }
-    // else if (delta_y < 0)
-    // {
-    //     env->real_min += ((env->real_max - env->real_min) * env->zoom->factor);
-    //     env->real_max -= ((env->real_max - env->real_min) * env->zoom->factor);
-    //     env->imaginary_min += ((env->imaginary_max- env->imaginary_min) * env->zoom->factor);
-    //     env->imaginary_max -= ((env->imaginary_max- env->imaginary_min) * env->zoom->factor);
-    // }
     update_pixel_size(env);
 	env->should_draw = true;
-    // print_boundaries(env);
-    // env->pixel_size += env->zoom->factor;
-    
-    
-    // printf("Heey I'm clicked\n");
-    // xdiff = env->zoom->xpos / (env->width / (env->real_max - env->real_min)) * -1 + env->real_max;
-    // ydiff = env->zoom->ypos / (env->height / (env->imaginary_max - env->imaginary_min)) * -1 + env->imaginary_max;
-    // printf("xdiff: %f || ydiff: %f\n", xdiff, ydiff);
-    // // env->zoom->real_center += xdiff / 2.0;
-    // // env->zoom->imaginary_center += ydiff / 2.0;
-    // interpolation = 1.0 / env->zoom->factor;
-    // env->real_min = (xdiff + ((env->real_min - xdiff) * interpolation));
-    // env->real_max = (xdiff + ((env->real_max - xdiff) * interpolation));
-    // env->imaginary_min = (ydiff + ((env->imaginary_min - ydiff) * interpolation));
-    // env->imaginary_max = (ydiff + ((env->imaginary_max - ydiff) * interpolation));
-    // env->real_min = env->zoom->real_center - (env->zoom->factor * (env->zoom->real_center - env->real_min));
-    // env->real_max = env->zoom->real_center + (env->zoom-> factor * (env->real_max - env->zoom->real_center));
-    // env->imaginary_min = env->zoom->imaginary_center - (env->zoom->factor * (env->zoom->imaginary_center - env->imaginary_min));
-    // env->imaginary_max = env->zoom->imaginary_center + (env->zoom->factor * (env->imaginary_max - env->zoom->imaginary_center));
-    // env->pixel_size = (double)(env->real_max - env->real_min) / env->width;
-    // compute_frame(env);
 }
 
 
