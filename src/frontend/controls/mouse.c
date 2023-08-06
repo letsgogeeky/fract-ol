dcba663efe5f0991994092af1aafe68bd55ddecc
Ramy <ramymohamedmousa@gmail.com>
2023-08-06 23:11:40 +0200

    updates to mouse zooming equations

--- src/frontend/controls/mouse.c
+++ src/frontend/controls/mouse.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mouse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ramoussa <ramoussa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ramymoussa <ramymoussa@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 23:29:15 by ramoussa          #+#    #+#             */
-/*   Updated: 2023/08/05 23:30:56 by ramoussa         ###   ########.fr       */
+/*   Updated: 2023/08/06 23:11:04 by ramymoussa       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ double interpolate(double start, double end, double interpolation)
 void mouse_scroll_hook(double delta_x, double delta_y, void * param)
 {
     t_fractol *env;
-    double mouse_real;
-    double mouse_imaginary;
     double interpolation;
     
     env = (t_fractol *)param;
@@ -32,13 +30,11 @@ void mouse_scroll_hook(double delta_x, double delta_y, void * param)
         env->zoom->factor = 0.97;
     else
         env->zoom->factor = 1.03;
-    mouse_real = env->zoom->xpos / (env->width / (env->real_max - env->real_min)) + env->real_min;
-    mouse_imaginary = env->zoom->ypos / (env->height / (env->imaginary_max - env->imaginary_min)) + env->imaginary_min;
     interpolation = 1.0 / env->zoom->factor;
-    env->real_min = interpolate(mouse_real, env->real_min, interpolation);
-    env->real_max = interpolate(mouse_real, env->real_max, interpolation);
-    env->imaginary_min = interpolate(mouse_imaginary, env->imaginary_min, interpolation);
-    env->imaginary_max =  interpolate(mouse_imaginary, env->imaginary_max, interpolation);
+    env->real_min = interpolate(env->zoom->real_center, env->real_min, interpolation);
+    env->real_max = interpolate(env->zoom->real_center, env->real_max, interpolation);
+    env->imaginary_min = interpolate(env->zoom->imaginary_center, env->imaginary_min, interpolation);
+    env->imaginary_max =  interpolate(env->zoom->imaginary_center, env->imaginary_max, interpolation);
     update_pixel_size(env);
 	env->should_draw = true;
 }
@@ -55,8 +51,6 @@ void mouse_cursor_hook(double xpos, double ypos, void* param)
     imaginary_range = env->imaginary_max - env->imaginary_min;
     env->zoom->xpos = xpos;
     env->zoom->ypos = ypos;
-    env->zoom->real_center = env->real_min + real_range * (env->width + env->zoom->xpos) / (2.0 * env->width);
-    env->zoom->imaginary_center = env->imaginary_min + imaginary_range * (env->height + env->zoom->ypos) / (2.0 * env->height);
-    // env->zoom->real_center = (xpos * env->pixel_size);
-    // env->zoom->imaginary_center = (ypos * env->pixel_size);
+    env->zoom->real_center = env->zoom->xpos / (env->width / real_range) + env->real_min;
+    env->zoom->imaginary_center = env->zoom->ypos / (env->height / imaginary_range) + env->imaginary_min;
 }
